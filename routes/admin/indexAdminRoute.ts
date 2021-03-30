e5a7926423d69628c05a6d8121f90759de048a24
alexis.savoie <alexis.savoie.555@gmail.com>
2021-03-30 17:02:46 +0200

    📦 NEW: Add login and middlewareSession

--- routes/admin/indexAdminRoute.ts
+++ routes/admin/indexAdminRoute.ts
@@ -5,6 +5,8 @@ import express from "express"
 // Import Routes
 import { testRouteAdmin } from "./otherRoutes/adminTestRoute"
 import { createAdminRoute } from "./authRoutes/createAdminRoute"
+import { authAdminRoute } from "./authRoutes/authAdminRoute"
+
 
 
 // Add imported routes to the admin app
@@ -12,5 +14,6 @@ const adminRoutes = express()
 
 adminRoutes.use("/", testRouteAdmin)
 adminRoutes.use("/", createAdminRoute)
+adminRoutes.use("/", authAdminRoute)
 
 export { adminRoutes }
