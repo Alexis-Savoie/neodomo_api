64fd82f3a8ba27e1a9cd418b385b9fae1d188218
alexis.savoie <alexis.savoie.555@gmail.com>
2021-04-19 10:23:13 +0200

    📦 NEW: added search related search

---
+++ manualTest/insertDatas.ts
@@ -0,0 +1,92 @@
+// Import models
+import { AdminModel } from "../models/adminModel"
+import { BillModel } from "../models/billModel"
+import { CommentModel } from "../models/commentModel"
+import { GamificationModel } from "../models/gamificationModel"
+
+import { LikeModel } from "../models/likeModel"
+import { MessageModel } from "../models/messageModel"
+import { PostModel } from "../models/postModel"
+import { ProductModel } from "../models/productModel"
+import { UserModel } from "../models/userModel"
+
+
+import bcrypt from 'bcrypt'
+
+
+
+
+let admin = new AdminModel({
+    emailAdmin: "alexis.savoie.555@gmail.com",
+    passwordAdmin: bcrypt.hashSync("bonjour11", bcrypt.genSaltSync(10)),
+})
+
+let bill = new BillModel({
+    numberBill: 9991,
+    emailBuyer: "searchBillRoute_user1@email.com",
+    description: "300 Domo",
+    price: 3.0,
+    paymentMethod: "CB",
+    idProduct: "vnozehvineozjoejajc"
+})
+
+let comment = new CommentModel( {
+    idPost: "dzefagrddvzqzdgerzvfzerg",
+    emailSender: "commentTest@email.com",
+    textContent: "Ceci est est un test de post incroyable",
+    replyTo: "dzefagrddvzqedgerzvfzerg",
+    haveReport: false
+})
+
+
+let gamification = new GamificationModel({
+    emailWinner: "searchGamificationRoute_user1@email.com",
+    levelGet: 10,
+})
+
+
+let message = new MessageModel({
+    emailSender: "messageTest@email.com",
+    emailReceiver: "searchMessageRoute_999@email.com",
+    textContent: "Ceci est est un test de message incroyable",
+    imageURL: "http://serveur.fr/image.png",
+})
+
+
+let post = new PostModel({
+    emailPublisher: "postTest@email.com",
+    textContent: "Ceci est est un test de post incroyableee",
+    listImage: [{ URL: "www.test.com/image.png" }],
+    listLike: [{ id: "hogzjovfzegvivzniovz" }],
+    listComment: [{ id: "hogzjovznjoivzniovz" }],
+    listReport: [{ id: "jotiobnvznvnzeinicae" }]
+})
+
+
+let product = new ProductModel({
+    nameProduct: "searchProductRoute_1",
+    description: "Ceci est est un test de produit incroyable",
+    price: 1000,
+    availableStock: 10,
+    imageURL: "www.test.com/image.png",
+    listBill: [{ id: "jotiobnvznvnzeinicae" }]
+})
+
+
+let user = new UserModel({
+    emailUser: "searchUserRoute_user1@email.com",
+    passwordUser: bcrypt.hashSync("bonjour11", bcrypt.genSaltSync(10)),
+})
+
+
+
+
+
+admin.save()
+bill.save()
+comment.save()
+gamification.save()
+message.save()
+post.save()
+product.save()
+user.save()
